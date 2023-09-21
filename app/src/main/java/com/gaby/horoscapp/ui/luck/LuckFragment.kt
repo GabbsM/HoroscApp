f317737e9c6369f75593f8940a44a15374e68cab
GabbsM <gabbsmoreno.gm@gmail.com>
2023-09-21 20:28:56 +0200

    add zodiac signes

--- app/src/main/java/com/gaby/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/gaby/horoscapp/ui/luck/LuckFragment.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.gaby.horoscapp.R
 import com.gaby.horoscapp.databinding.FragmentLuckBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class LuckFragment : Fragment() {
+
 
     private var _binding : FragmentLuckBinding? = null
     private val binding get() = _binding!!
